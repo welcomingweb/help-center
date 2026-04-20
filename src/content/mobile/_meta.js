430974a1b553eb5ee9eb86ccac5c353e52f3c36b
Alişan Erdemli <alisan.erdemli@gobito.com>
2026-04-20 20:20:23 +0300

    Add files via upload

---
+++ src/content/mobile/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  "index": "Overview",
+  "getting-started": "Getting Started",
+  "platforms": "Platforms",
+  "rules": "Accessibility Rules",
+  "panel": "Accessibility Panel",
+  "ci-cd": "CI / CD Integration"
+};
